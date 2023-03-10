9de256591d3e6b7715105ca09d9bc67455762dab
Tim Mitra <tim@it-guy.com>
2023-03-10 17:45:03 -0500

    add Location Tests and remove example tests

---
+++ ToDoTests/LocationTests.swift
@@ -0,0 +1,21 @@
+//
+//  LocationTests.swift
+//  ToDoTests
+//
+//  Created by Tim Mitra on 2023-03-10.
+//
+
+import XCTest
+@testable import ToDo
+
+final class LocationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
