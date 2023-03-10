eb95cd1762ab4ba48868ff9997fc1b12b4d6dc08
Tim Mitra <tim@it-guy.com>
2023-03-10 18:15:37 -0500

    add ToDoItemStore tests

---
+++ ToDo/ToDoItemStore.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoItemStore.swift
+//  ToDo
+//
+//  Created by Tim Mitra on 2023-03-10.
+//
+
+import Foundation
+
+class ToDoItemStore {
+  
+}
---
+++ ToDoTests/ToDoItemStoreTests.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoItemStoreTests.swift
+//  ToDoTests
+//
+//  Created by Tim Mitra on 2023-03-10.
+//
+
+import XCTest
+@testable import ToDo
+
+final class ToDoItemStoreTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+  func test_add_shouldPublishChange() {
+    let sut = ToDoItemStore()
+  }
+}
