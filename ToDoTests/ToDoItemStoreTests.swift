b5398a1e9385ce153c48dd89a729f48438e3dc58
Tim Mitra <tim@it-guy.com>
2023-03-18 22:55:00 -0400

    added id to ToDoItem for comparison test Check item added file & line with helper function to make errors easier to understand

--- ToDoTests/ToDoItemStoreTests.swift
+++ ToDoTests/ToDoItemStoreTests.swift
@@ -10,29 +10,45 @@ import XCTest
 import Combine
 
 final class ToDoItemStoreTests: XCTestCase {
+  
+  var sut: ToDoItemStore!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = ToDoItemStore()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = ToDoItemStore()
     }
 
   func test_add_shouldPublishChange() throws {
-    let sut = ToDoItemStore()
     let todoItem = ToDoItem(title: "Dummy")
     let receivedItems = try wait(for: sut.itemPublisher) {
       sut.add(todoItem)
     }
     XCTAssertEqual(receivedItems, [todoItem])
   }
+  
+  func test_check_shouldPublishChangeInDoneItems() throws {
+    let sut = ToDoItemStore()
+    let todoItem = ToDoItem(title: "Dummy")
+    sut.add(todoItem)
+    sut.add(ToDoItem(title: "Dummy 2"))
+    let receivedItems = try wait(for: sut.itemPublisher) {
+      sut.check(todoItem)
+    }
+    let doneItems = receivedItems.filter({ $0.done })
+    XCTAssertEqual(doneItems, [todoItem])
+  }
 }
 
 extension XCTestCase {
   func wait<T: Publisher>(
     for publisher: T,
-    afterChange change: () -> Void) throws -> T.Output where T.Failure == Never {
+    afterChange change: () -> Void,
+    file: StaticString = #file,
+    line: UInt = #line) throws
+  -> T.Output where T.Failure == Never {
     let publisherExpectation = expectation(
       description: "Wait for publisher in \(#file)"
     )
@@ -48,7 +64,9 @@ extension XCTestCase {
     token.cancel()
     let unwrappedResult = try XCTUnwrap(
       result,
-    "Publisher did not publish any value"
+    "Publisher did not publish any value",
+      file: file,
+      line: line
     )
   return unwrappedResult
   }
