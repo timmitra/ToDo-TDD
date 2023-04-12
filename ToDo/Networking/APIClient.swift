c271b27c11a8c1539af56c24e1c9728db3a13f54
Tim Mitra <tim@it-guy.com>
2023-04-11 20:38:58 -0400

    With this implementation, our app can now fetch the coordinate of an address string. This feature enables our users to add a location to a to-do item.

--- ToDo/Networking/APIClient.swift
+++ ToDo/Networking/APIClient.swift
@@ -6,18 +6,38 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol APIClientProtocol {
   func coordinate(
     for: String,
-    completion: (Coordinate?) -> Void
+    completion: @escaping (Coordinate?) -> Void
   )
 }
 
 class APIClient: APIClientProtocol {
+  
+  lazy var geocoder: GeoCoderProtocol = CLGeocoder()
+  
   func coordinate(
-    for: String,
-    completion: (Coordinate?) -> Void) {
-    
+    for address: String,
+    completion: @escaping (Coordinate?) -> Void) {
+      geocoder.geocodeAddressString(address) {
+        placemarks, error in
+        guard let clCoordinate = placemarks?.first?.location?.coordinate
+        else {
+          completion(nil)
+          return
+        }
+        let coordinate = Coordinate(latitude: clCoordinate.latitude, longitude: clCoordinate.longitude)
+        completion(coordinate)
+      }
   }
 }
+
+protocol GeoCoderProtocol {
+  func geocodeAddressString(
+  _ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler)
+}
+
+extension CLGeocoder: GeoCoderProtocol { }
--- ToDoTests/APIClientTests.swift
+++ ToDoTests/APIClientTests.swift
@@ -7,6 +7,8 @@
 
 import XCTest
 @testable import ToDo
+import Intents
+import Contacts
 
 final class APIClientTests: XCTestCase {
   
@@ -19,4 +21,28 @@ final class APIClientTests: XCTestCase {
     override func tearDownWithError() throws {
       sut = nil
     }
+  
+  func test_coordinate_fetchesCoordinate() {
+    let geoCoderMock = GeoCoderProtocolMock()
+    sut.geocoder = geoCoderMock
+    let location = CLLocation(latitude: 1, longitude: 2)
+    let placemark = CLPlacemark(location: location, name: nil, postalAddress: nil)
+    let expectedAddress = "dummy address"
+    var result: Coordinate?
+    sut.coordinate(for: "") { coordinate in
+     result = coordinate
+    }
+   geoCoderMock.completionHandler?([placemark], nil)
+    XCTAssertEqual(result?.latitude, location.coordinate.latitude)
+    XCTAssertEqual(result?.longitude, location.coordinate.longitude)
+  }
+  
+  func test_coordinate_shouldCallGeoCoderWithAddress() {
+    let geoCoderMock = GeoCoderProtocolMock()
+    sut.geocoder = geoCoderMock
+    let expectedAddress = "dummy address"
+    sut.coordinate(for: expectedAddress) { _ in
+    }
+    XCTAssertEqual(geoCoderMock.geocodeAddressString, expectedAddress)
+  }
 }
