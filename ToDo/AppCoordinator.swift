75feec0b53560e724b265a887673c3da41a7066d
Tim Mitra <tim@it-guy.com>
2023-04-11 22:22:27 -0400

    In the initializer of AppCoordinator, we store the window that was passed in as a parameter, and we set up the view of the initial view controller. In the start method, we set the rootViewController property of the window.

--- ToDo/AppCoordinator.swift
+++ ToDo/AppCoordinator.swift
@@ -10,3 +10,16 @@ import UIKit
 protocol Coordinator {
   func start()
 }
+
+class AppCoordinator: Coordinator {
+  private let window: UIWindow?
+  private let viewController: UIViewController
+  init(window: UIWindow?) {
+    self.window = window
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    viewController = storyboard.instantiateViewController(withIdentifier: "ToDoItemsListViewController")
+  }
+  func start() {
+    window?.rootViewController = viewController
+  }
+}
