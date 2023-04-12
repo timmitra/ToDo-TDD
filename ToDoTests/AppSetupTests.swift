9fa08733dd7bb24e62d3890e587fd006087f8f99
Tim Mitra <tim@it-guy.com>
2023-04-11 23:00:50 -0400

    forgot to add throws

--- ToDoTests/AppSetupTests.swift
+++ ToDoTests/AppSetupTests.swift
@@ -10,11 +10,13 @@ import XCTest
 
 class AppSetupTests: XCTestCase {
 
-  func test_application_shouldSetupRoot() {
+  func test_application_shouldSetupRoot() throws {
     let application = UIApplication.shared
     let scene = application.connectedScenes.first as? UIWindowScene
     let root = scene?.windows.first?.rootViewController
-    XCTAssert(root is ToDoItemsListViewController)
+    let nav = try XCTUnwrap(root as?
+        UINavigationController)
+    XCTAssert(nav.topViewController is ToDoItemsListViewController)
   }
 
 }
